653a4bd72e45e7eb694e038a74063a477194547f
Tanaka <ndhlovutanaka02@gmail.com>
2025-05-22 01:16:06 +0200

    Set up Tailwind CSS and Shadcn UI with button, input, form, and card components

--- smart-student-handbook/src/App.jsx
+++ smart-student-handbook/src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+
+    
     <>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Hello, Tailwind!</h1>
+    </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
