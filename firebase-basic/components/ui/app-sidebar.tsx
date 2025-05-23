52431413102fae2c0a0cf0f16b1aff68797d0939
Reinhard <dijkstraalgorithm07@gmail.com>
2025-05-23 14:10:36 +0200

    Added Dark Mode

--- firebase-basic/components/ui/app-sidebar.tsx
+++ firebase-basic/components/ui/app-sidebar.tsx
@@ -13,7 +13,17 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Switch } from "@/components/ui/switch"
 
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
 
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 // Menu items.
 const items = [
@@ -45,10 +55,12 @@ const items = [
 ]
 
 export function AppSidebar() {
+  const { setTheme } = useTheme()
+
   return (
     <Sidebar>
       <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png"/>
+        <AvatarImage src="https://github.com/shadcn.png" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
 
@@ -68,11 +80,31 @@ export function AppSidebar() {
                 </SidebarMenuItem>
               ))}
             </SidebarMenu>
-            <Switch/>
+            <Switch />
 
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon">
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme("light")}>
+            Light
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("dark")}>
+            Dark
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme("system")}>
+            System
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </Sidebar>
   )
 }
