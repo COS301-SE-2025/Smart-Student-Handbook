001d17e8b9d62b2e33c7c274cc4fe52bccd9e526
Reinhard <dijkstraalgorithm07@gmail.com>
2025-05-28 14:38:42 +0200

    Update quilleditor.tsx

--- firebase-basic/components/quilleditor.tsx
+++ firebase-basic/components/quilleditor.tsx
@@ -1,8 +1,34 @@
-// components/QuillEditor.tsx
+'use client';
+
+import { useEffect, useRef } from 'react';
 import dynamic from 'next/dynamic';
 import 'react-quill-new/dist/quill.snow.css';
 
 const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, false] }],
+    ['bold', 'italic', 'underline', 'strike'],
+    [{ color: [] }, { background: [] }],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    [{ script: 'sub' }, { script: 'super' }],
+    [{ indent: '-1' }, { indent: '+1' }],
+    [{ align: [] }],
+    ['blockquote', 'code-block'],
+    ['link', 'image', 'video'],
+    ['clean'],
+  ],
+};
+
+const formats = [
+  'header', 'font',
+  'bold', 'italic', 'underline', 'strike',
+  'color', 'background',
+  'list', 'indent', 'script', 'align',
+  'blockquote', 'code-block',
+  'link', 'image', 'video',
+];
 
 export default function QuillEditor({
   value,
@@ -11,13 +37,32 @@ export default function QuillEditor({
   value: string;
   onChange: (val: string) => void;
 }) {
+  const registered = useRef(false);
+
+  useEffect(() => {
+    if (!registered.current) {
+      const Quill = require('react-quill-new');
+
+      const Font = Quill.Quill.import('formats/font');
+      Font.whitelist = ['sans-serif', 'serif', 'monospace', 'roboto', 'arial', 'times-new-roman'];
+      Quill.Quill.register(Font, true);
+
+      const List = Quill.Quill.import('formats/list');
+      Quill.Quill.register(List, true);
+
+      registered.current = true;
+    }
+  }, []);
+
   return (
-    <div className="editor-content border p-4 rounded bg-white shadow-sm h-[400px] overflow-y-auto">
+    <div className="editor-content border p-4 rounded bg-white shadow-sm h-[400px]">
       <ReactQuill
         value={value}
         onChange={onChange}
         theme="snow"
-        className="h-full"
+        modules={modules}
+        formats={formats}
+        className="h-full [&_.ql-container]:h-full [&_.ql-editor]:h-full [&_.ql-editor]:overflow-y-auto"
       />
     </div>
   );
