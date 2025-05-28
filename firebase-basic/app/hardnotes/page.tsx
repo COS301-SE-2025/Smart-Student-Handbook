aec9ec30c5a411dc66d23cae1d870c7d6cceb7c5
Reinhard <dijkstraalgorithm07@gmail.com>
2025-05-28 12:43:19 +0200

    Believe

--- firebase-basic/app/hardnotes/page.tsx
+++ firebase-basic/app/hardnotes/page.tsx
@@ -9,21 +9,39 @@ import {
     FolderIcon,
     Trash2,
 } from "lucide-react";
-import { Card, CardContent } from "@/components/ui/card";
-import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
 
 import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
 
-const Quill = dynamic(() => import("react-quill"), { ssr: false });
-
 import QuillEditor from "@/components/quilleditor";
-const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
 type EditorProps = {
     content: string;
     onContentChange: (content: string) => void;
+};
+
+const toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ header: 1 }, { header: 2 }],                   // custom button values
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  [{ script: 'sub' }, { script: 'super' }],         // superscript/subscript
+  [{ indent: '-1' }, { indent: '+1' }],             // outdent/indent
+  [{ direction: 'rtl' }],                            // text direction
+
+  [{ size: ['small', false, 'large', 'huge'] }],    // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ color: [] }, { background: [] }],              // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ['clean'],                                        // remove formatting button
+];
+
+const modules = {
+  toolbar: toolbarOptions,
 };
 
 type Note = {
