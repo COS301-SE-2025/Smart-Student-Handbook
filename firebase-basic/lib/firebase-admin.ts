980b51182a89fc8ed18473eb100916af9ad951b0
MagundaTakudzwa <u22599160@tuks.co.za>
2025-06-18 22:19:11 +0200

    Remove JSON import; load Firebase Admin creds from env

--- firebase-basic/lib/firebase-admin.ts
+++ firebase-basic/lib/firebase-admin.ts
@@ -1,12 +1,21 @@
+
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getDatabase } from "firebase-admin/database";
-import serviceAccount from "./serviceAccountKey.json"; // This file must also exist!
 
-const app = getApps().length
-  ? getApps()[0]
-  : initializeApp({
-      credential: cert(serviceAccount as any),
-      databaseURL: "https://studenthandbook-a215a-default-rtdb.europe-west1.firebasedatabase.app",
-    });
+/** Decode the base-64 JSON string, if provided */
+const serviceAccount =
+  process.env.FIREBASE_SERVICE_ACCOUNT &&
+  JSON.parse(
+    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf8")
+  );
+
+const app =
+  getApps().length > 0
+    ? getApps()[0]
+    : initializeApp({
+        credential: cert(serviceAccount as any),
+        databaseURL:
+          "https://studenthandbook-a215a-default-rtdb.europe-west1.firebasedatabase.app",
+      });
 
 export const adminDb = getDatabase(app);
