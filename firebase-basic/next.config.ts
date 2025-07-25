1bdfd73c9113b914045ec545687a12d9a0dbb780
Tanaka <ndhlovutanaka02@gmail.com>
2025-07-25 04:56:35 +0200

    updated firebase-admin

--- firebase-basic/next.config.ts
+++ firebase-basic/next.config.ts
@@ -7,6 +7,15 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  env: {
+    NEXT_PUBLIC_FIREBASE_DATABASE_URL: 'https://dummy-default-rtdb.firebaseio.com/',
+    NEXT_PUBLIC_FIREBASE_API_KEY: 'dummy',
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: 'dummy.firebaseapp.com',
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: 'dummy',
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: 'dummy.appspot.com',
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: 'dummy',
+    NEXT_PUBLIC_FIREBASE_APP_ID: 'dummy',
+  },
 };
 
 export default nextConfig;
