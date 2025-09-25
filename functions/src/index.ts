a33063ae699e924f22578f02ec31402bd57b6141
Tanaka <ndhlovutanaka02@gmail.com>
2025-09-25 13:18:41 +0200

    update index.tx

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,6 @@ export * from "./lectures";
 export * from "./events";
 export * from "./semesters";
 export * from "./organizations";
-
+export * from "./friends";
 export * from "./orgFlashcards";  // flashcards only
 export * from "./orgSummaries";   // summaries only
